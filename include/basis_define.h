2b211e3b778b5e6b14e6a4015fabbfe08fc796a6
anzhh <azh885522@sina.com>
2015-09-25 18:45:18 +0800

    port observer from poco

---
+++ include/basis_define.h
@@ -0,0 +1,118 @@
+#ifndef _BASIS_DEFINE_H_
+#define _BASIS_DEFINE_H_
+
+#if !defined(__WINDOWS__) && !defined(__POSIX__)
+#if defined(_MSC_VER) || defined(WIN32) || defined(_WIN32)
+#define __WINDOWS__   1
+#elif defined(__linux__)
+#define __POSIX__  1
+#elif defined(__APPLE__)
+#define __POSIX__  1
+#else
+#error "Unrecognized OS platform"
+#endif
+#endif//!defined(__WINDOWS__) && !defined(__POSIX__)
+
+#if (defined(_DEBUG) || defined(DEBUG)) && !defined(__DEBUG__)
+#define __DEBUG__        1    // debug version
+#endif
+
+#if defined(_MSC_VER)
+# pragma warning(disable:4996) // VC++ 2008 deprecated function warnings
+# pragma warning(disable:4200) // warning C4200: nonstandard extension used : zero-sized array in struct/union
+# pragma warning(disable:4355) // warning C4355: 'this': used in base member initializer list
+# pragma warning(disable:4819) // warning C4819: The file contains a character that cannot be represented in the current code page (936)
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
+
+#include <vector>
+#include <string>
+#include <list>
+#include <set>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+#ifdef __WINDOWS__
+#ifndef _INC_WINDOWS
+#include <WinSock2.h>
+#endif /* _INC_WINDOWS */
+#endif//__WINDOWS__
+
+#ifdef __POSIX__
+#include <unistd.h>
+#include <stdint.h>       // int64_t, uint64_t
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <sys/times.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <sys/statvfs.h>
+#include <sys/wait.h>
+#include <sys/file.h>
+#include <sys/ioctl.h>
+#include <sys/poll.h>
+#include <sys/epoll.h> // linuxÃÿ”–
+#include <pthread.h>
+#include <semaphore.h>
+#include <typeinfo>
+#include <iconv.h>
+//#include <linux/types.h>
+#endif//__POSIX__
+
+#ifdef __WINDOWS__
+typedef unsigned __int8      byte;
+typedef __int8               int8;
+typedef unsigned __int8      uint8;
+typedef __int16              int16;
+typedef unsigned __int16     uint16;
+typedef __int32              int32;
+typedef unsigned __int32     uint32;
+typedef __int64              int64;
+typedef unsigned __int64     uint64;
+typedef float                float32;
+typedef double               float64;
+typedef int                  socklen_t;
+typedef unsigned long        pthread_t;
+#endif//__WINDOWS__
+
+#ifdef __POSIX__
+typedef uint8_t              byte;
+typedef int8_t               int8;
+typedef uint8_t              uint8;
+typedef int16_t              int16;
+typedef uint16_t             uint16;
+typedef int32_t              int32;
+typedef uint32_t             uint32;
+typedef int64_t              int64;
+typedef uint64_t             uint64;
+typedef float                float32;
+typedef double               float64;
+typedef unsigned char        BYTE;
+typedef unsigned short       WORD;
+typedef unsigned long        DWORD;
+typedef int                  SOCKET;
+typedef int                  BOOL;
+typedef int                  HANDLE;
+#define TRUE                 (1)
+#define FALSE                (0)
+#define INVALID_SOCKET       (SOCKET)(~0)
+#define INVALID_HANDLE_VALUE (HANDLE)(-1)
+#define SOCKET_ERROR         (-1)
+#define __stdcall
+#endif//__POSIX__
+
+#endif //_BASIS_DEFINE_H_
---
+++ include/basis_observer_ext.h
@@ -0,0 +1,87 @@
+#ifndef _BASIS_OBSERVER_EXT_H_
+#define _BASIS_OBSERVER_EXT_H_
+
+#include "basis_observer.h"
+
+namespace basis {
+
+
+template <class C, class N>
+class BSObserverExt: public BSObserver
+{
+public:
+	typedef void (C::*Callback)(N*);
+
+	BSObserverExt(C* object, Callback method): 
+		m_object(object), 
+		m_callbk(method)
+	{
+	}
+	
+	BSObserverExt(const BSObserverExt& observer):
+		BSObserver(observer),
+		m_object(observer.m_object), 
+		m_callbk(observer.m_callbk)
+	{
+	}
+	
+	virtual ~BSObserverExt()
+	{
+	}
+	
+	BSObserverExt& operator = (const BSObserverExt& observer)
+	{
+		if (&observer != this)
+		{
+			m_object = observer.m_object;
+			m_callbk  = observer.m_callbk;
+		}
+		return *this;
+	}
+	
+	void notify(BSNotification* pnotification) const
+	{
+		if (m_object)
+		{
+			N* pcastnf = dynamic_cast<N*>(pnotification);
+			if (pcastnf)
+			{
+				(m_object->*m_callbk)(pcastnf);
+			}
+		}
+	}
+	
+	bool equals(const BSObserver* observer) const
+	{
+		const BSObserverExt* pobs = dynamic_cast<const BSObserverExt*>(observer);
+		return pobs && pobs->m_object == m_object && pobs->m_callbk == m_callbk;
+	}
+
+	bool accepts(BSNotification* pnotification) const
+	{
+		return dynamic_cast<N*>(pnotification) != NULL;
+	}
+	
+	BSObserverExt* clone() const
+	{
+		return new BSObserverExt(*this);
+	}
+	
+	void disable()
+	{
+		m_object = NULL;
+	}
+	
+private:
+	BSObserverExt();
+
+	C*       m_object;
+	Callback m_callbk;
+	// lock ?????
+};
+
+
+} // namespace basis
+
+
+#endif // _BASIS_OBSERVER_EXT_H_
