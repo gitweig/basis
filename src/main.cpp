650ddd7293bc295563c1e60b0c6dd1e97e059057
anzhh <azh885522@sina.com>
2015-11-12 10:19:54 +0800

    delete write copy

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,5 @@
 int main()
 {
 	BSSubject aaa;
-	
-	string a1 = "aaaaa";
-	string a2 = "aaaaa";
-	string a3 = a1;
-
-	printf("%X\n", a1.c_str());
-	printf("%X\n", a2.c_str());
-	printf("%X\n", a3.c_str());
-
 	return 0;
 }
