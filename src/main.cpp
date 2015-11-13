06410a82d74256bb5dde6ecc802e66530a092803
anzhh <azh885522@sina.com>
2015-11-13 15:48:32 +0800

    transplant scoped_ptr&weak_ptr&smart_ptr from boost and xcore

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,38 @@
 #include "basis_observer.h"
 #include "basis_observer_ext.h"
 #include "basis_subject.h"
+#include "basis_smart_ptr.h"
+#include "stdio.h"
+
+class TestA
+{
+public:
+	TestA() 
+	{
+		printf("construct\n");
+	}
+
+	~TestA()
+	{
+		printf("destruct\n");
+	}
+};
 
 int main()
 {
-	BSSubject aaa;
+	basis::weak_ptr<TestA> b, c;
+	{
+		basis::shared_ptr<TestA> a = shared_ptr<TestA>(new TestA);
+		b = a;
+		c = b;
+		bool expried = b.expired();
+		bool expriedc = c.expired();
+		int e = expried;
+	}
+	
+	bool expried = b.expired();
+	bool expriedc = c.expired();
+
+	
 	return 0;
 }
