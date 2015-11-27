3374c58e942922eeb9bdc81eaf64f6db03d1a336
anzhh <azh885522@sina.com>
2015-11-27 17:28:20 +0800

    fix macro

--- include/basis_macro.h
+++ include/basis_macro.h
@@ -27,19 +27,11 @@ namespace basis
 	abort(); \
 	} \
 	} while (0)
-//nullptr
-#define ASSERTPTR(ptr) \
+
+#define CHECKPTR(ptr) \
 	do{ \
 	if (ptr == NULL) { \
-	fprintf(stderr, "Assertion ("#ptr") failed at %s(..) in %s:%d, LastError:%u\r\n", __FUNCTION__, __FILE__, __LINE__, ::GetLastError()); \
-	fflush(stderr); \
-	abort(); \
-	} \
-	} while(0)
-#define VERIFYPTR(ptr) \
-	do{ \
-	if (ptr == NULL) { \
-	fprintf(stderr, "Verification ("#ptr") failed at %s(..) in %s:%d, LastError:%u\r\n", __FUNCTION__, __FILE__, __LINE__, ::GetLastError()); \
+	fprintf(stderr, "CheckPtr ("#ptr") is NULL at %s(..) in %s:%d, LastError:%u\r\n", __FUNCTION__, __FILE__, __LINE__, ::GetLastError()); \
 	fflush(stderr); \
 	abort(); \
 	} \
@@ -64,31 +56,23 @@ namespace basis
 	} \
 	} while (0)
 
-#define ASSERTPTR(ptr) \
+#define CHECKPTR(ptr) \
 	do { \
 	if (ptr == NULL) {\
-	fprintf(stderr, "Assertion ("#ptr") failed at %s(..) in %s:%d, errno:%d\r\n", __FUNCTION__, __FILE__, __LINE__, errno); \
+	fprintf(stderr, "Assertion ("#ptr") is NULL at %s(..) in %s:%d, errno:%d\r\n", __FUNCTION__, __FILE__, __LINE__, errno); \
 	fflush(stderr); \
 	abort(); \
-	}
+	} \
 	} while (0)
-#define VERIFYPTR(ptr) \
-	do 	{ \
-	if (ptr == NULL) { \
-	fprintf(stderr, "Verification ("#ptr") failed at %s(..) in %s:%d, errno:%d\r\n", __FUNCTION__, __FILE__, __LINE__, errno); \
-	fflush(stderr); \
-	abort(); \
-	}
-	} while (0);
+
 #endif//__WINDOWS__
 #else//__DEBUG__
 #define ASSERT(x)
 #define VERIFY(x) (x)
-#define ASSERTPTR(ptr)
-#define VERIFYPTR(ptr) (ptr)
+#define CHECKPTR(ptr)
 #endif//__DEBUG__
 
-}// namespace basis
+}; // namespace basis
 
 using namespace basis;
 #endif //_BASIS_MACRO_H_
