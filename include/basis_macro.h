88fea7b2b22a2e9e7acbe8c9ebe011a9127b8d47
wgs <wgs@163.com>
2015-11-27 09:38:21 +0800

    basis maro

--- include/basis_macro.h
+++ include/basis_macro.h
@@ -1,9 +1,92 @@
 #ifndef _BASIS_MACRO_H_
 #define _BASIS_MACRO_H_
 
+#include "basis_define.h"
 
-#define ASSERT(a) 
-#define VERIFY(a) a
+namespace basis
+{
+///////////////////////////////////////////////////////////////////////////////
+#undef ASSERT
+#undef VERIFY
+#ifdef __DEBUG__
+#ifdef __WINDOWS__
+#define ASSERT(x) \
+	do { \
+	if (!(x)) { \
+	fprintf(stderr, "Assertion ("#x") failed at %s(..) in %s:%d, LastError:%u\r\n", __FUNCTION__, __FILE__, __LINE__, ::GetLastError()); \
+	fflush(stderr); \
+	abort(); \
+	} \
+	} while (0)
 
+#define VERIFY(x) \
+	do { \
+	if (!(x)) { \
+	fprintf(stderr, "Verification ("#x") failed at %s(..) in %s:%d, LastError:%u\r\n", __FUNCTION__, __FILE__, __LINE__, ::GetLastError()); \
+	fflush(stderr); \
+	abort(); \
+	} \
+	} while (0)
+//nullptr
+#define ASSERTPTR(ptr) \
+	do{ \
+	if (ptr == NULL) { \
+	fprintf(stderr, "Assertion ("#ptr") failed at %s(..) in %s:%d, LastError:%u\r\n", __FUNCTION__, __FILE__, __LINE__, ::GetLastError()); \
+	fflush(stderr); \
+	abort(); \
+	} \
+	} while(0)
+#define VERIFYPTR(ptr) \
+	do{ \
+	if (ptr == NULL) { \
+	fprintf(stderr, "Verification ("#ptr") failed at %s(..) in %s:%d, LastError:%u\r\n", __FUNCTION__, __FILE__, __LINE__, ::GetLastError()); \
+	fflush(stderr); \
+	abort(); \
+	} \
+	} while(0)
 
+#else//__WINDOWS__
+#define ASSERT(x) \
+	do { \
+	if (!(x)) { \
+	fprintf(stderr, "Assertion ("#x") failed at %s(..) in %s:%d, errno:%d\r\n", __FUNCTION__, __FILE__, __LINE__, errno); \
+	fflush(stderr); \
+	abort(); \
+	} \
+	} while (0)
+
+#define VERIFY(x) \
+	do { \
+	if (!(x)) { \
+	fprintf(stderr, "Verification ("#x") failed at %s(..) in %s:%d, errno:%d\r\n", __FUNCTION__, __FILE__, __LINE__, errno); \
+	fflush(stderr); \
+	abort(); \
+	} \
+	} while (0)
+
+#define ASSERTPTR(ptr) \
+	do { \
+	if (ptr == NULL) {\
+	fprintf(stderr, "Assertion ("#ptr") failed at %s(..) in %s:%d, errno:%d\r\n", __FUNCTION__, __FILE__, __LINE__, errno); \
+	fflush(stderr); \
+	abort(); \
+	}
+	} while (0)
+#define VERIFYPTR(ptr) \
+	do 	{ \
+	if (ptr == NULL) { \
+	fprintf(stderr, "Verification ("#ptr") failed at %s(..) in %s:%d, errno:%d\r\n", __FUNCTION__, __FILE__, __LINE__, errno); \
+	fflush(stderr); \
+	abort(); \
+	}
+	} while (0);
+#endif//__WINDOWS__
+#else//__DEBUG__
+#define ASSERT(x)
+#define VERIFY(x) (x)
+#endif//__DEBUG__
+
+}// namespace basis
+
+using namespace basis;
 #endif //_BASIS_MACRO_H_
