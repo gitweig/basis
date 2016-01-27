d508ca4582bcefcf37327a9a07bdaa291dc41b6b
wgs <wgs@163.com>
2016-01-27 11:16:07 +0800

    src

--- src/test.cpp
+++ src/test.cpp
@@ -10,14 +10,21 @@ using namespace basis;
 BSSyncQueue<int> syncqueue;
 
 BSEvent the_event;
+BSAtomic32 the_count;
 
 class Run1 : public BSRunnable
 {
 public:
 	virtual void run(BSThread* p)
 	{
-		the_event.wait();
-		printf("11");
+		while (!p->wait_quit(0))
+		{
+			syncqueue.push(1);
+			if (the_count++ > 20000000)
+			{
+				break;
+			}
+		}
 	}
 };
 
@@ -26,45 +33,38 @@ class Run2 : public BSRunnable
 public:
 	virtual void run(BSThread* p)
 	{
-		the_event.wait();
-		printf("22");
+		while (!p->wait_quit(0))
+		{
+			int value = 0;
+			syncqueue.pull(value);
+			if (syncqueue.size() == 0)
+			{
+				break;
+			}
+		}
 	}
 };
 
 
 int main()
 {
-	BSThread thread1("t1");
-	BSThread thread2("t2");
 
-	Run1 r1;
-	Run2 r2;
+	time_t t = time(0);
+	char tmp[64];
+	strftime( tmp, sizeof(tmp), "%Y/%m/%d %X %A 本年第%j天 %z", localtime(&t) );
 
-	thread1.start(&r1);
-	thread2.start(&r2);
+	SYSTEMTIME sys;
+	GetLocalTime( &sys );
+	printf( "%4d/%02d/%02d %02d:%02d:%02d.%03d 星期%1d\n", sys.wYear,   sys.wMonth,  sys.wDay, sys.wHour, sys.wMinute, sys.wSecond,sys.wMilliseconds,sys.wDayOfWeek);
+
+	LARGE_INTEGER m_nFreq;
+	LARGE_INTEGER m_nTime;
+	QueryPerformanceFrequency(&m_nFreq); // 获取时钟周期
+	QueryPerformanceCounter(&m_nTime);//获取当前时间
+	printf(" time:%lld us",(m_nTime.QuadPart*1000000/m_nFreq.QuadPart));//m_nFreq.QuadPart为:次数/s，这样就可以获得毫秒级别的了
 
 	Sleep(10000);
-	
-	the_event.set();
-
-	int a = 0;
-	syncqueue.pull(a);
-	syncqueue.pull(a);
-	
-
-
-
-	//thread1.stop();
-	//thread2.stop();
-
-	Sleep(100000);
-	thread1.join();
-	thread2.join();
-
-	
-	uint32 sz = syncqueue.size();
-	
-
+	getchar();
 	return 0;
 }
 	
